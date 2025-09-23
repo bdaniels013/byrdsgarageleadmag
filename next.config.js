53d3a6f6849ed5ecaf81d86b2df2b371b82f629c
Mac <user@Macs-Mac-Pro.attlocal.net>
2025-09-23 12:06:52 -0500

    Fix Next.js config warning

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
